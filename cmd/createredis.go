f1d6861f93434fb3d5c3da0447dc8ae1838bcfa7
Jack Xie <jxie@riotgames.com>
2019-03-13 15:32:42 -0700

    init

--- cmd/createredis.go
+++ cmd/createredis.go
@@ -17,8 +17,46 @@ package cmd
 import (
 	"fmt"
 
+	tcloudredis "github.com/rdxsl/tcloud-provisioner/tencent-cloud/redis"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
+
+var redisConfName string
+
+func redisParseConfig(tm *tcloudredis.TcloudRedis) error {
+	switch viper.Get("Instance").(type) {
+	case float64:
+		tm.Instance = int64(viper.Get("Instance").(float64))
+	default:
+		return fmt.Errorf("mysql config needs to have 'Instance' with type 'int' without quotes, currently we have %+v\n", viper.Get("Instance"))
+	}
+	switch viper.Get("Memory").(type) {
+	case float64:
+		tm.Memory = int64(viper.Get("Memory").(float64))
+	default:
+		return fmt.Errorf("mysql config needs to have 'Memory' with type 'int' without quotes, currently we have %+v\n", viper.Get("Memory"))
+	}
+	switch viper.Get("Volume").(type) {
+	case float64:
+		tm.Volume = int64(viper.Get("Volume").(float64))
+	default:
+		return fmt.Errorf("mysql config needs to have 'Volume' with type 'int' without quotes, currently we have %+v\n", viper.Get("Volume"))
+	}
+	switch viper.Get("Region").(type) {
+	case string:
+		tm.Region = string(viper.Get("Region").(string))
+	default:
+		return fmt.Errorf("mysql config needs to have 'Region' with type 'string' with quotes, currently we have %+v\n", viper.Get("Region"))
+	}
+	switch viper.Get("Zone").(type) {
+	case string:
+		tm.Zone = string(viper.Get("Zone").(string))
+	default:
+		return fmt.Errorf("mysql config needs to have 'Zone' with type 'string' with quotes, currently we have %+v\n", viper.Get("Zone"))
+	}
+	return nil
+}
 
 // createCmd represents the create command
 var createRedisCmd = &cobra.Command{
@@ -31,12 +69,30 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("create called")
+		viper.SetConfigFile(redisConfName)
+		err := viper.ReadInConfig()
+		if err != nil {
+			fmt.Println(fmt.Errorf("Fatal error config file: %s \n", err))
+		}
+		var tr tcloudredis.TcloudRedis
+		err = redisParseConfig(&tr)
+		tr.Create()
+
+		// if err == nil {
+		// 	tm.Create()
+		// } else {
+		// 	fmt.Println(err)
+		// }
+		// fmt.Println(viper.Get("Instance"))
+		// tcloudredis.TcloudRedis()
 	},
 }
 
 func init() {
 	redisCmd.AddCommand(createRedisCmd)
+	// createRedisCmd.Flags().StringVarP(&redisConfName, "conf", "c", "", "location of the redis conf json file")
+
+	createRedisCmd.Flags().StringVarP(&redisConfName, "env", "e", "", "sub directory inside of ./conf what has a region's config files")
 
 	// Here you will define your flags and configuration settings.
 
